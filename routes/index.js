ff356f666efc1b51e228eece855f10c17cf28809
cipher1729 <cipher1729@gmail.com>
2015-10-01 23:25:00 -0700

    Sample post application on index

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*POST with some data*/
+router.post('/',  function(req, res, next) {
+  res.render('index', { first: req.body.first, second:req.body.second, third:req.body.third});
+ console.log(req.body.first);
+ console.log(req.body.second);
+ console.log(req.body.third);
+  });
+
 module.exports = router;
